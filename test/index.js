400a3fcaaf4b025530936f982bebc1e1bd894fdb
Graham Lutz <graham@brainbytescreative.com>
2017-07-23 12:32:49 -0400

    adds basic graves route and test

--- test/index.js
+++ test/index.js
@@ -68,3 +68,21 @@ describe('User Routes', () => {
 		})
 	})
 })
+
+describe('Graves Routes', () => {
+	before(()=> {
+		// insert Graham
+			// insert actuarial data
+		// insert James
+			// insert actuarial data
+	})
+	after(()=> {
+		
+	})
+	it('should return accurate grave data', done => {
+		request.get('http://localhost:1701/graves/1', (err, res, body) => {
+			assert.equal(JSON.parse(body).deathProbabilityPercentage, 0.2123);
+			done();
+		})
+	})
+})
