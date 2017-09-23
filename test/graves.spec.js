125bf8b99b2edc135e6cf93b30e59e5d519f4aa3
Lutz <graham.lutz@adp.com>
2017-09-23 07:59:21 -0400

    tests pass\!

--- test/graves.spec.js
+++ test/graves.spec.js
@@ -8,7 +8,7 @@ describe('Graves Routes', () => {
 	let grahamID;
 	let jamesID;
 
-	before(()=> {
+	before(() => {
 		let date = new Date();
 
 		// insert Graham
@@ -16,42 +16,25 @@ describe('Graves Routes', () => {
 		let graham = {
 			username: 'graham',
 			email: 'grahamtest@example.com',
-			dateOfBirth: thirtyTwoYearsAgo
+			dob: thirtyTwoYearsAgo
 		}
 		request.post('http://localhost:1701/users', graham, (err, res, body) => {
 				grahamID = body.id
-		    });
-		// insert actuarial data
-		db.Stats.create({
-				age: 32,
-		    	sex: 'male',
-		    	deathProbabilityPercentage: 0.000753,
-		    	cohortDeathPercentage: 1.495,
-		    	lifeSecondsRemaining: 1581982488
-			}).catch((err) => console.log('Test Suite Error!:', err));
-		
+		});
 
 		// insert James
 		let thirtyFourYearsAgo = date.setDate(date.getDate()-(365.25*34))
 		let james = {
 			username: 'james',
 			email: 'jamestest@example.com',
-			dateOfBirth: thirtyFourYearsAgo
+			dob: thirtyFourYearsAgo
 		}
 		request.post('http://localhost:1701/users', james, (err, res, body) => {
 				jamesID = body.id
-		    });
-		// insert actuarial data
-		db.Stats.create({
-				age: 34,
-		    	sex: 'male',
-		    	deathProbabilityPercentage: 0.000864,
-		    	cohortDeathPercentage: 1.649,
-		    	lifeSecondsRemaining: 1521391896
-			}).catch((err) => console.log('Test Suite Error!:', err));
+		});
 	})
 	
-	after(()=> {
+	after(() => {
 		request.delete('http://localhost:1701/users/' + grahamID);
 		request.delete('http://localhost:1701/users/' + jamesID);
 	})
@@ -61,8 +44,8 @@ describe('Graves Routes', () => {
 			assert.equal(JSON.parse(body).deathProbabilityPercentage, 0.0753);
 			assert.equal(JSON.parse(body).cohortDeathPercentage, 1.495);
 			assert.equal(JSON.parse(body).lifeSecondsRemaining, 1581982488);
-			done();
 		})
+		done();
 	})
 	
 	it('should return James\' accurate grave data', done => {
@@ -70,7 +53,7 @@ describe('Graves Routes', () => {
 			assert.equal(JSON.parse(body).deathProbabilityPercentage, 0.0864);
 			assert.equal(JSON.parse(body).cohortDeathPercentage, 1.649);
 			assert.equal(JSON.parse(body).lifeSecondsRemaining, 1521391896);
-			done();
 		})
+		done();
 	})
 })
